3a6e0cc253b517353d0a60fa1c2e572e31caed58
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-25 09:29:39 -0700

    Add getters and setters for PartsOfSpeech

---
+++ MadLibs/Models/PartsOfSpeech.cs
@@ -0,0 +1,10 @@
+namespace MadLibs.Models
+{
+  public class PartsOfSpeech
+  {
+    public string Adjective { get; set; }
+    public string Noun { get; set; }
+    public string Verb { get; set; }
+    public string AdjectiveTwo { get; set; }
+  }
+}
